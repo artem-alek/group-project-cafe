38e15ed21191bdc09610238701fb85f11f0ce880
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-22 17:12:45 -0500

    fixed merge conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
+import { makeBoxes } from './tab-boxes';
 import {fillHtml } from './templates';
 import { reservation } from './res-data';
 
 //$(".container").html(fillHtml());
 $(".reserve").click(reservation);
+$('.tab-container').append(makeBoxes);
