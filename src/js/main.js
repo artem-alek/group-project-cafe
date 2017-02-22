0b3b4516855deb23ccbf45152f6b7080de3be79b
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-22 17:11:34 -0500

    bug fix in main.js' '

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,5 @@ import $ from 'jquery';
 import {fillHtml } from './templates';
 import { reservation } from './res-data';
 
-$(".container").html(fillHtml());
+//$(".container").html(fillHtml());
 $(".reserve").click(reservation);
