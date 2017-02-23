e3f232662c18552532c39884889f720331d0e01c
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-23 13:59:43 -0500

    completed menu tab

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ import $ from 'jquery';
 import { makeBoxes, makeForm } from './tab-boxes';
 import {fillHtml } from './templates';
 import { reservation } from './res-data';
+import { getMenu } from './apiCalls';
+import { menuButton } from './menuData';
 
 $('.tab-container').append(makeBoxes);
 $('.reservation-tab').click(makeForm);
+$('.menu-tab').click(menuButton);
--- src/js/menuData.js
+++ src/js/menuData.js
@@ -1,46 +1,29 @@
 import $ from 'jquery';
 import {menuTemplate, menuTitle} from './templates';
+import { getMenu } from './apiCalls';
 
 function sortMenu(items) {
-  Object.keys(items).forEach(function (key) {
+    Object.keys(items).forEach(function (key) {
     pullData(items[key], key);
   });
-  // for (var prop in items) {
-  //   pullData(items[prop], prop);
-  // }
-  // pullData(items.appetizers, 'appetizers');
-  // pullData(items.entrees);
-  // pullData(items.sides);
+
 }
 
   function pullData (menu, title) {
-    $('.testing').append(menuTitle(title));
+    //console.log(menu, title)
+    $('.tab-content').append(menuTitle(title));
     for (var i=0; i < menu.length; i++) {
       var data = menu[i];
       var html = menuTemplate(data);
-      $('.testing').append(html);
+      $('.tab-content').append(html);
     }
   }
-  // for (var i =0; i < items.appetizers.length; i++) {
-  //   var product = items.appetizers[i];
-  //   var html = menuTemplate(product);
-  //   $('.testing').append(html);
-  // }
-  // for (var i =0; i < items.entrees.length; i++) {
-  //   var product = items.entrees[i];
-  //   var html = menuTemplate(product);
-  //   $('.testing').append(html);
-  // }
-  // for (var i =0; i < items.sides.length; i++) {
-  //   var product = items.sides[i];
-  //   var html = menuTemplate(product);
-  //   $('.testing').append(html);
-  // }
+
+function menuButton (event) {
+  $(".tab-content").empty();
+   getMenu().then(sortMenu);
+   }
 
 
 
-
-
-
-
-export {sortMenu};
+export {sortMenu, pullData, menuButton};
