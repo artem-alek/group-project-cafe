02e94addfde195b2dbb2b3c6cdf0695fc74c30df
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-23 10:01:02 -0500

    finished form and submission button and proper main.js function calls

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 import $ from 'jquery';
-import { makeBoxes } from './tab-boxes';
+import { makeBoxes, makeForm } from './tab-boxes';
 import {fillHtml } from './templates';
 import { reservation } from './res-data';
 
-//$(".container").html(fillHtml());
-$(".reserve").click(reservation);
 $('.tab-container').append(makeBoxes);
+$('.reservation-tab').click(makeForm);
