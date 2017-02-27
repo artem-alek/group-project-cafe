89beb2b3bfced620eeb9621483aa1668ac2edb8d
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-26 20:37:27 -0500

    fininshed all edits and css.

--- src/js/location.js
+++ src/js/location.js
@@ -1,7 +1,7 @@
 function locationTemplate () {
   return `
-    <div class = "loction-title"> Our Location </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.340805936194!2d-84.393532248631!3d33.75185808059409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f50383f3ab72e5%3A0xab2ff0008509bc09!2sThe+Iron+Yard+-+Atlanta!5e0!3m2!1sen!2sus!4v1487883071532" width="100%" height="auto" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class = "location-title"> Our Location </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.340805936194!2d-84.393532248631!3d33.75185808059409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f50383f3ab72e5%3A0xab2ff0008509bc09!2sThe+Iron+Yard+-+Atlanta!5e0!3m2!1sen!2sus!4v1487883071532" width="100%" height="200px" frameborder="0" style="border:0" allowfullscreen></iframe>
     <div class = "address"> 115 M.L.K. Jr Dr SW #400, Atlanta, GA 30303 </div>
   `;
 }
